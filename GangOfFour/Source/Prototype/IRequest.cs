ab87421240d952f1f771acceb09db25d0724ca64
kirill <gobixmwork@gmail.com>
2015-07-14 17:26:09 +0300

    added prototype pattern

---
+++ GangOfFour/Source/Prototype/IRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Prototype
+{
+    internal interface IRequest
+    {
+        IRequest Clone();
+        void Modify(Func<char, char> replace);
+        char[] Data { get; }
+    }
+}
---
+++ GangOfFour/Source/Prototype/Request.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Prototype
+{
+    internal class Request : IRequest
+    {
+        private char[] _data;
+
+        public char[] Data
+        {
+            get { return _data; }
+        }
+
+        public Request(char[] data)
+        {
+            _data = data;
+        }
+
+        public IRequest Clone()
+        {
+            return new Request((char[])_data.Clone());
+        }
+
+        public void Modify(Func<char, char> replace)
+        {
+            for (int i = 0; i < Data.Length; i++)
+            {
+                _data[i] = replace(_data[i]);
+            }
+        }
+    }
+}
