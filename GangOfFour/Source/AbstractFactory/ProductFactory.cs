e550776704c0567885cff3e8d032237117664034
kirill <gobixmwork@gmail.com>
2015-07-14 14:31:01 +0300

    resharped

--- GangOfFour/Source/AbstractFactory/ProductFactory.cs
+++ GangOfFour/Source/AbstractFactory/ProductFactory.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Infotecs.GangOfFour.AbstractFactory
+﻿namespace Infotecs.GangOfFour.AbstractFactory
 {
     internal abstract class ProductFactory
     {
