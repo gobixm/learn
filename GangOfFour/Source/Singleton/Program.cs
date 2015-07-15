b7ef977068ca203e9ac746d2062a90b77b12530e
kirill <gobixmwork@gmail.com>
2015-07-15 09:30:53 +0300

    Singleton pattern added

---
+++ GangOfFour/Source/Singleton/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Infotecs.GangOfFour.Singleton
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Singleton.Instance.Field);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GangOfFour/Source/Singleton/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Infotecs.GangOfFour.Singleton
+{
+    internal sealed class Singleton
+    {
+        private static readonly Singleton _instance = new Singleton();
+
+        static Singleton()
+        {
+        }
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get { return _instance; }
+        }
+
+        public string Field
+        {
+            get { return "Singleton " + GetType().FullName; }
+        }
+    }
+}
