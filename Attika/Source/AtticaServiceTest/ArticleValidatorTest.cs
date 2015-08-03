9c53d6bb54284cb2ba35bf808dae4d87af410c74
kirill <gobixmwork@gmail.com>
2015-08-03 13:01:27 +0300

    * unit tests refactored

--- Attika/Source/AtticaServiceTest/ArticleValidatorTest.cs
+++ Attika/Source/AtticaServiceTest/ArticleValidatorTest.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using Infotecs.Attika.AttikaService.Validators;
 using Infotecs.Attika.AttikaSharedDataObjects.DataTransferObjects;
 using Xunit;
@@ -9,36 +7,78 @@ namespace AtticaServiceTest
 {
     public class ArticleValidatorTest
     {
-        [Theory]
-        [InlineData(101)]
-        private void TestInvalidArticleTitle(int titleLength)
+        private string GetStringWithDesiredLength(int length)
         {
-            var title = new string('x', titleLength);
-            var article = new ArticleDto {Title = title};
-            string[] errors;
-            var valid = article.Validate(out errors, true);
-            Assert.False(valid, string.Join(";",errors));
+            return new string('x', length);
         }
 
         [Theory]
-        [InlineData(201)]
-        private void TestInvalidArticleText(int textLength)
+        [InlineData(100)]
+        [InlineData(0)]
+        private void Check_ArticleTitle_Valid(int titleLength)
         {
-            var text = new string('x', textLength);
-            var article = new ArticleDto { Title = text };
+            string title = GetStringWithDesiredLength(titleLength);
+            var article = new ArticleDto {Title = title};
             string[] errors;
-            var valid = article.Validate(out errors, true);
+            bool valid = article.Validate(out errors, true);
+            Assert.True(valid);
+        }
+
+        [Theory]
+        [InlineData(101)]
+        private void Check_ArticleTitle_Invalid(int titleLength)
+        {
+            string title = GetStringWithDesiredLength(titleLength);
+            var article = new ArticleDto {Title = title};
+            string[] errors;
+            bool valid = article.Validate(out errors, true);
             Assert.False(valid, string.Join(";", errors));
         }
 
         [Theory]
-        [InlineData(51)]
-        private void TestInvalidArticleCommentText(int textLength)
+        [InlineData(0)]
+        [InlineData(200)]
+        private void Check_ArticleText_Valid(int textLength)
         {
-            var text = new string('x', textLength);
-            var article = new ArticleDto { Title = text, Comments = new List<CommentDto>(){new CommentDto(){Text = text}}};
+            string text = GetStringWithDesiredLength(textLength);
+            var article = new ArticleDto {Text = text};
             string[] errors;
-            var valid = article.Validate(out errors, true);
+            bool valid = article.Validate(out errors, true);
+            Assert.True(valid);
+        }
+
+        [Theory]
+        [InlineData(201)]
+        private void Check_ArticleText_Invalid(int textLength)
+        {
+            string text = GetStringWithDesiredLength(textLength);
+            var article = new ArticleDto {Text = text};
+            string[] errors;
+            bool valid = article.Validate(out errors, true);
+            Assert.False(valid, string.Join(";", errors));
+        }
+
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(50)]
+        private void Check_ArticleCommentText_Valid(int textLength)
+        {
+            string text = GetStringWithDesiredLength(textLength);
+            var article = new ArticleDto {Comments = new List<CommentDto> {new CommentDto {Text = text}}};
+            string[] errors;
+            bool valid = article.Validate(out errors, true);
+            Assert.True(valid, string.Join(";", errors));
+        }
+
+        [Theory]
+        [InlineData(51)]
+        private void Check_ArticleCommentText_Invalid(int textLength)
+        {
+            string text = GetStringWithDesiredLength(textLength);
+            var article = new ArticleDto {Comments = new List<CommentDto> {new CommentDto {Text = text}}};
+            string[] errors;
+            bool valid = article.Validate(out errors, true);
             Assert.False(valid, string.Join(";", errors));
         }
     }
--- Attika/Source/AtticaServiceTest/CommentValidatorTest.cs
+++ Attika/Source/AtticaServiceTest/CommentValidatorTest.cs
@@ -6,15 +6,32 @@ namespace AtticaServiceTest
 {
     public class CommentValidatorTest
     {
+        private string GetStringWithDesiredLength(int length)
+        {
+            return new string('x', length);
+        }
+
         [Theory]
         [InlineData(51)]
-        public void TestCommentValidationRules(int commentLength)
+        public void Check_CommentText_Invalid(int commentLength)
         {
-            var text = new string('x', commentLength);
+            string text = GetStringWithDesiredLength(commentLength);
             var comment = new CommentDto {Text = text};
             string[] errors;
             bool valid = comment.Validate(out errors);
             Assert.False(valid, string.Join(";", errors));
+        }
+
+        [Theory]
+        [InlineData(50)]
+        [InlineData(0)]
+        public void Check_CommentText_Valid(int commentLength)
+        {
+            string text = GetStringWithDesiredLength(commentLength);
+            var comment = new CommentDto {Text = text};
+            string[] errors;
+            bool valid = comment.Validate(out errors);
+            Assert.True(valid, string.Join(";", errors));
         }
     }
 }
