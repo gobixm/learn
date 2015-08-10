b74b515ed5424b17a8444ee14fcce6e1b73b70e6
kirill <gobixmwork@gmail.com>
2015-08-10 17:55:58 +0300

    * opus added

---
+++ Attika.v2/Source/AttikaConsoleHost/Environment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Infotecs.Opus;
+using NLog;
+
+namespace Infotecs.Attika.AttikaConsoleHost
+{
+    public class Environment
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public static void DumpInfo()
+        {
+            IConfiguration dumper = OpusDumper.Configure((x) =>
+            {
+                x.Include(InformationKind.ApplicationInfo);
+                x.Include(InformationKind.NetFrameworkInstalled);
+                x.Include(InformationKind.OperatingSystem);
+            });
+
+            Logger.Info(dumper.Dump());
+        }
+    }
+}
--- Attika.v2/Source/AttikaConsoleHost/Program.cs
+++ Attika.v2/Source/AttikaConsoleHost/Program.cs
@@ -2,31 +2,15 @@
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using Infotecs.Attika.AttikaConsoleHost.Configurations.ApplicationRoots;
-using Infotecs.Opus;
-using NLog;
 using Nelibur.ServiceModel.Services.Default;
 
 namespace Infotecs.Attika.AttikaConsoleHost
 {
     public class Program
     {
-        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
-
-        private static void DumpEnvironmentInfo()
-        {
-            IConfiguration dumper = OpusDumper.Configure((x) =>
-            {
-                x.Include(InformationKind.ApplicationInfo);
-                x.Include(InformationKind.NetFrameworkInstalled);
-                x.Include(InformationKind.OperatingSystem);
-            });
-
-            Logger.Info(dumper.Dump());
-        }
-
         private static void Main(string[] args)
         {
-            DumpEnvironmentInfo();
+            Environment.DumpInfo();
             using (NinjectServiceLocator.Kernel)
             {
                 Console.WriteLine("Message based service");
