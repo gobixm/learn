58c291d7e928e9858906df795e2da72a9e5420f1
kirill <gobixmwork@gmail.com>
2015-08-14 17:41:52 +0300

    fixed

--- Attika.v2/Source/AttikaDomainTests/CommentValidatorTest.cs
+++ Attika.v2/Source/AttikaDomainTests/CommentValidatorTest.cs
@@ -20,7 +20,7 @@ namespace Infotecs.Attika.AttikaDomainTests
 
         [Theory]
         [InlineData(0)]
-        [InlineData(51)]
+        [InlineData(50)]
         public void Check_CommentTitle_Valid(int textLength)
         {
             string text = GetStringWithDesiredLength(textLength);
