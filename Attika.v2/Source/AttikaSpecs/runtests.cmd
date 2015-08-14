b46e40d6af7a27c0f5e9afb35cdf5b3c9a6eb065
kirill <gobixmwork@gmail.com>
2015-08-14 11:45:16 +0300

    msbuild

--- Attika.v2/Source/AttikaSpecs/runtests.cmd
+++ Attika.v2/Source/AttikaSpecs/runtests.cmd
@@ -1,15 +1,15 @@
 @pushd %~dp0
 
-%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe "AttikaSpecs.csproj"
+MSBuild.exe "AttikaSpecs.csproj"
 
 @if ERRORLEVEL 1 goto end
 
-@cd ..\packages\SpecRun.Runner.*\tools
+@cd ..\..\packages\SpecRun.Runner.*\tools
 
 @set profile=%1
 @if "%profile%" == "" set profile=Default
 
-SpecRun.exe run %profile%.srprofile "/baseFolder:%~dp0\bin\Debug" /log:specrun.log %2 %3 %4 %5
+SpecRun.exe run %profile%.srprofile /baseFolder:"../../../Out/Release/Attika.Specs" /log:specrun.log %2 %3 %4 %5
 
 :end
 
