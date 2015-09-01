03c615d52b5d7dfe86aaa9dd274181ef24d47909
gobi <gobixm@gmail.com>
2015-09-01 20:47:28 +0300

    stored procedure added and renamed

---
+++ FancyDelivery/Source/FancyDatabase/dbo/StoredProcedures/GetProductCountGroupedByCategory.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[GetProductCountGroupedByCategory]
+AS
+SELECT c.Id, c.Name, COUNT(p.Id)
+	FROM dbo.Product p 
+	INNER JOIN dbo.Category c on c.Id = p.CategoryId
+	GROUP BY C.ID, c.Name
+
