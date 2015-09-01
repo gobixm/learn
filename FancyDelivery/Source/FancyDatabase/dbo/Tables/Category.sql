9127785484d2b07c4983a2f877553add9da18bfe
gobi <gobixm@gmail.com>
2015-09-01 19:51:57 +0300

    database project initial commit

---
+++ FancyDelivery/Source/FancyDatabase/dbo/Tables/Category.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Category] (
+    [Id]   INT           IDENTITY (1, 1) NOT NULL,
+    [Code] NVARCHAR (20) NULL,
+    [Name] NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ FancyDelivery/Source/FancyDatabase/dbo/Tables/Product.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Product] (
+    [Id]          INT             IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (50)   NULL,
+    [Description] NVARCHAR (250)  NULL,
+    [ImageName]   NVARCHAR (250)  NULL,
+    [Price]       DECIMAL (19, 5) NULL,
+    [CategoryId]  INT             NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK1F94D86AEE4E26ED] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Category] ([Id]) ON DELETE CASCADE
+);
+
