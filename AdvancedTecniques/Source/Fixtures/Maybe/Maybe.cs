44b6905a03694c4746882ecf36b53337a17123b2
gobi <gobixm@gmail.com>
2015-09-26 18:14:41 +0300

    +return default

--- AdvancedTecniques/Source/Fixtures/Maybe/Maybe.cs
+++ AdvancedTecniques/Source/Fixtures/Maybe/Maybe.cs
@@ -9,5 +9,11 @@ namespace Fixtures.Maybe
         {
             return o == null ? null : evaluator(o);
         }
+
+        public static TResult Return<TInput, TResult>(this TInput o, Func<TInput, TResult> evaluator, TResult defaultValue)
+            where TResult : class where TInput : class
+        {
+            return o == null ? defaultValue : evaluator(o);
+        }
     }
 }
--- AdvancedTecniques/Source/Tests/MaybeTest.cs
+++ AdvancedTecniques/Source/Tests/MaybeTest.cs
@@ -5,20 +5,40 @@ namespace Tests
 {
     public class MaybeTest
     {
+        private const string WellDone = "well done";
+
         [Test]
         public void ShouldNotThrowNull()
         {
-            const string expected = "well done";
-            var hierarchy = new MaybeFixture();
-            hierarchy.SpawnChild()
-                .SpawnChild()
-                .Value = expected;
-            Assert.AreEqual(expected, hierarchy.Child.Child.Value);
-            hierarchy.Child = null;
+            var hierarchy = BuildFixture();
+            BrakeFixture(hierarchy);
             Assert.IsNull(hierarchy.With(x => x.Child)
                 .With(x => x.Child)
                 .With(x => x.Value));
+        }
 
+        [Test]
+        public void ShouldReturnDefaultValue()
+        {
+            var hierarchy = BuildFixture();
+            BrakeFixture(hierarchy);
+            Assert.AreEqual("default", hierarchy.With(x => x.Child)
+                .With(x => x.Child)
+                .Return(x=>x.Value, "default"));
+        }
+
+        private static void BrakeFixture(MaybeFixture hierarchy)
+        {
+            hierarchy.Child = null;
+        }
+
+        private static MaybeFixture BuildFixture()
+        {
+            var hierarchy = new MaybeFixture();
+            hierarchy.SpawnChild()
+                .SpawnChild()
+                .Value = WellDone;
+            return hierarchy;
         }
     }
 }
