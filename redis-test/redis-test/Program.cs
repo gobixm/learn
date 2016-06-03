2689451ea647171cff8e09f877245ca6d109c777
Kirill Ivanov <gobixm@gmail.com>
2016-06-04 00:02:59 +0300

    performance testing

--- redis-test/redis-test/Program.cs
+++ redis-test/redis-test/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using StackExchange.Redis;
 
 namespace redis_test
@@ -9,24 +13,85 @@ namespace redis_test
         private static void Main(string[] args)
         {
             var options = ConfigurationOptions.Parse("localhost");
-            options.SyncTimeout = 10000;
+            options.AllowAdmin = true;
+            options.ResponseTimeout = 1000;
+            options.SyncTimeout = 1000;
             //options.WriteBuffer = 400 * 1024 * 1024;
-            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(options);
-            IDatabase db = redis.GetDatabase();
-            byte[] data = new byte[300 * 1024 * 1024];
-            for (int i = 0; i < 10* 1024; i++)
+            var redis = ConnectionMultiplexer.Connect(options);
+            var endpoint = redis.GetEndPoints(true).Single();
+            var server = redis.GetServer(endpoint);
+            server.FlushDatabase();
+            server.FlushDatabase(1);
+            var db = redis.GetDatabase();
+            var dbRead = redis.GetDatabase(1);
+
+
+
+            var sw = Stopwatch.StartNew();
+            var generator = new MatrixGenerator();
+            var side = 100000;
+            generator.Generate(side, (index, data) => { db.StringSet($"matrix:{index}", data); });
+            Console.WriteLine($"write. done in {sw.Elapsed} at {side / sw.Elapsed.TotalSeconds} writes/sec");
+
+            //modify every bit in row
+            sw.Restart();
+            for (int i = 0; i < side; i++)
             {
-                Stopwatch sw = Stopwatch.StartNew();
-                db.HashSet("org", i, data);
-                Console.WriteLine($"publish - {sw.Elapsed}");
-                sw.Restart();
-                var incoming = (byte[])db.HashGet("org", i);
-                Console.WriteLine($"consume {incoming.Length} bytes - {sw.Elapsed}");
-                sw.Restart();
-                db.HashDelete("org", i);
-                Console.WriteLine($"remove - {sw.Elapsed}");
+                db.StringSetBit($"matrix:{0}", i, true);
             }
-            Console.WriteLine("done");
+            Console.WriteLine($"modify every single bit in row. done in {sw.Elapsed} at {side / sw.Elapsed.TotalSeconds} writes/sec");
+
+            //modify bit in every row
+            sw.Restart();
+            for (int i = 0; i < side; i++)
+            {
+                db.StringSetBit($"matrix:{i}", i, true);
+            }
+            Console.WriteLine($"modify single bit in every row. done in {sw.Elapsed} at {side / sw.Elapsed.TotalSeconds} writes/sec");
+
+            //modify bit in every row in batch
+            var tasks = new Task[side];
+            sw.Restart();
+            var batch = db.CreateBatch();
+            for (int i = 0; i < side; i++)
+            {
+                tasks[i] = batch.StringSetBitAsync($"matrix:{i}", i, false);
+            }
+            batch.Execute();
+            redis.WaitAll(tasks);
+            Console.WriteLine($"modify single bit in every row in batch. done in {sw.Elapsed} at {side / sw.Elapsed.TotalSeconds} writes/sec");
+
+            //read all rows
+            sw.Restart();
+            int size = 0;
+            for (int i = 0; i < side; i++)
+            {
+                size += ((byte[])db.StringGet($"matrix:{i}")).Length;
+            }
+            Console.WriteLine($"read all rows. done in {sw.Elapsed} at {side / sw.Elapsed.TotalSeconds} reads/sec total {size} bytes");
+
+            //read all rows async
+            sw.Restart();
+            var reads = new Task[side];
+            for (int i = 0; i < side; i++)
+            {
+                reads[i] = db.StringGetAsync($"matrix:{i}");
+            }
+            Task.WaitAll(reads);
+            Console.WriteLine($"read all rows async. done in {sw.Elapsed} at {side / sw.Elapsed.TotalSeconds} reads/sec");
+
+            //clone db
+            sw.Restart();
+
+            var clones = new List<Task>(side);
+            foreach (var redisKey in server.Keys(0, "matrix:*"))
+            {
+                var task = db.KeyDumpAsync(redisKey).ContinueWith(t => dbRead.KeyRestoreAsync(redisKey, t.Result));
+                clones.Add(task);
+            }
+            Task.WaitAll(clones.ToArray());
+            Console.WriteLine($"clone all rows. done in {sw.Elapsed} at {side / sw.Elapsed.TotalSeconds} clone/sec");
+
             Console.ReadKey();
         }
     }
