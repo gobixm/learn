c59e2b8689478d8e89093a219d520eb9af1b3a51
gobixm <gobixm@gmail.com>
2017-10-19 11:27:02 +0300

    learning

---
+++ Nats/Consumer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var receiver = new Receiver(Guid.NewGuid().ToString()))
+            {
+                receiver.Start();
+                Task.Delay(1000000000).Wait();
+            }
+        }
+    }
+}
---
+++ Nats/Consumer/Receiver.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using STAN.Client;
+
+namespace Consumer
+{
+    public class Receiver : IDisposable
+    {
+        private readonly IStanConnection connection;
+        private readonly Stopwatch stopwatch = new Stopwatch();
+        private long messages = 0;
+        private Timer timer;
+
+        public Receiver(string id)
+        {
+            var stanConnectionFactory = new StanConnectionFactory();
+            var options = StanOptions.GetDefaultOptions();
+            connection = stanConnectionFactory.CreateConnection("test-cluster", id, options);
+            timer = new Timer(Timer, null, 0, 1000);
+        }
+
+        private void Timer(object state)
+        {
+            Console.WriteLine($"{messages/((double)stopwatch.ElapsedMilliseconds/1000)} messages/sec");
+            messages = 0;
+            stopwatch.Restart();
+        }
+
+        public void Start()
+        {
+            var options = StanSubscriptionOptions.GetDefaultOptions();
+            options.StartWithLastReceived();
+            options.MaxInflight = 1000;
+            connection.Subscribe("topic",
+                "consumer-group",
+                options,
+                (sender, args) =>
+                {
+                    messages++;
+                });
+        }
+
+        public void Dispose()
+        {
+            connection?.Dispose();
+        }
+    }
+}
